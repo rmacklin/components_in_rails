3a502442f64e2977f3bc8ebebeac05cc82890e18
Richard Macklin <richard.github@nrm.com>
2019-06-03 16:31:35 -0700

    Add HelperTestCase base class

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,11 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
+
+module Components
+  class HelperTestCase < ActionView::TestCase
+    def assert_ltrim_dom_equal(expected, resulted)
+      assert_dom_equal expected.gsub(/^\s+/, '').gsub(/\n/, ''), resulted.gsub(/^\s+/, '').gsub(/\n/, '')
+    end
+  end
+end
