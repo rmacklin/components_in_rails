2fe335ca58a76342635994741ad5b056642c7423
Richard Macklin <richard.github@nrm.com>
2019-06-03 16:34:54 -0700

    Add render_example_code helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def render_example_code(path)
+    path = Pathname.new(path)
+
+    unless path.absolute?
+      path = Rails.root.join('app/views', path.dirname, "_#{path.basename}.html.erb")
+    end
+
+    File.read(path)
+  end
 end
